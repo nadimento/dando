50e53da1a1f6153dac2fd2f737f3608a521d9acb
nadimento <nadimento@gmail.com>
2025-04-27 05:38:36 -0700

    supabase fix

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ihpynnheqgphpglgldbi.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlocHlubmhlcWdwaHBnbGdsZGJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3MTg2NTksImV4cCI6MjA2MTI5NDY1OX0.vNKh1vcCPL0GxI9igmmuYmCH4dBYnjBvxTxaVCT13uM";
-
+const supabaseKey = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
