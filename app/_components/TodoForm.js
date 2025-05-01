1016fcd837f8f577108b5c34be5498f63c9e1fdf
nadimento <nadimento@gmail.com>
2025-05-01 01:39:34 -0700

    error clears when input is changed

--- app/_components/TodoForm.js
+++ app/_components/TodoForm.js
@@ -7,12 +7,12 @@ import TodoList from "./TodoList";
 
 export default function TodoForm({ data }) {
   const [error, setError] = useState("");
+  const formRef = useRef(null);
+
   const [optimisticTodos, optimisticAddTodo] = useOptimistic(
     data,
     (currentTodos, newTodo) => [...currentTodos, newTodo],
   );
-
-  const formRef = useRef(null);
 
   async function handleSubmit(formData) {
     const form = formRef.current;
@@ -48,6 +48,9 @@ export default function TodoForm({ data }) {
           name="task"
           type="text"
           placeholder="Enter your task"
+          onChange={() => {
+            if (error) setError("");
+          }}
           className="mt-1 block w-full rounded-xl border-2 border-red-300 bg-rose-50 px-4 py-3 text-rose-400 shadow-sm placeholder:text-rose-400 focus:border-rose-500 focus:ring-0 focus:outline-none"
         />
         {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
